0bbf037cf1fcc307882f3480290f9efd3b2e7a36
devmeist3r <devmeist3r@gmail.com>
2021-02-05 14:00:39 -0300

    *

--- 09 - Cap/NavigationAPI/app/src/main/java/com/devmeist3r/navigationapi/CompleteFragment.kt
+++ 09 - Cap/NavigationAPI/app/src/main/java/com/devmeist3r/navigationapi/CompleteFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class HomeFragment : Fragment() {
+class CompleteFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_complete, container, false)
     }
 
 }

94e999e7c5a28691778daf358d1fea00d5c272fb
devmeist3r <devmeist3r@gmail.com>
2021-02-06 04:29:45 -0300

    *

---
+++ 10 - Cap/Persistencia/app/src/main/java/com/devmeist3r/persistencia/MainActivity.kt
@@ -0,0 +1,194 @@
+package com.devmeist3r.persistencia
+
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnPermissionDenied
+import permissions.dispatcher.RuntimePermissions
+import java.io.*
+import java.lang.Exception
+
+@RuntimePermissions
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnRead.setOnClickListener {
+            btnReadClick()
+        }
+
+        btnSave.setOnClickListener {
+            btnSaveClick()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    @OnPermissionDenied(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun showDeniedForExternal() {
+        Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun btnReadClick() {
+        val type = rgType.checkedRadioButtonId
+
+        when (type) {
+            R.id.rbInternal -> loadFromInternal()
+            R.id.rbExtenalPriv -> loadFromExternal(true)
+            R.id.rbExternalPublic -> loadFromExternal(false)
+        }
+
+    }
+
+    private fun btnSaveClick() {
+        val type = rgType.checkedRadioButtonId
+
+        when (type) {
+            R.id.rbInternal -> saveToInternal()
+            R.id.rbExtenalPriv -> saveToExternal(true)
+            R.id.rbExternalPublic -> saveToExternal(false)
+        }
+    }
+
+    private fun saveToInternal() {
+        try {
+            val fos = openFileOutput("arquivo.txt", Context.MODE_PRIVATE)
+            save(fos)
+        } catch (e: Exception) {
+            Log.e("NGVL", "saveToInternal: Erro ao salvar", e)
+        }
+    }
+
+    private fun loadFromInternal() {
+        try {
+            val fis = openFileInput("arquivo.txt")
+            load(fis)
+        } catch (e: Exception) {
+            Log.e("NGVL", "loadFromInternal: Error ao carregar arquivo", e)
+        }
+    }
+
+    @NeedsPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    fun loadFromExternal(privateDir: Boolean) {
+//        val hasPermission = checkStoragePermission(android.Manifest.permission.READ_EXTERNAL_STORAGE, RC_STORAGE_PERMISSION)
+//
+//        if (!hasPermission) {
+//            return
+//        }
+
+        val state = Environment.getExternalStorageState()
+
+        if (Environment.MEDIA_MOUNTED == state ||
+            Environment.MEDIA_MOUNTED_READ_ONLY == state) {
+            val myDir = getExternalDir(privateDir)
+
+            if (myDir?.exists() == true) {
+                val txtFile = File(myDir, "arquivo.txt")
+                if(txtFile.exists()) {
+                    try {
+                        txtFile.createNewFile()
+                        val fis = FileInputStream(txtFile)
+                        load(fis)
+                    } catch (e: Exception) {
+                        Log.d("NGVL", "loadFromExternal: Erro ao carregar arquivo", e)
+                    }
+                }
+            }
+        } else {
+            Log.e("NGVL", "loadFromExternal: SD Card indisponível")
+        }
+    }
+
+    private fun getExternalDir(privateDir: Boolean) = if(privateDir) getExternalFilesDir(null)
+    else Environment.getExternalStorageDirectory()
+
+
+    @NeedsPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun saveToExternal(privateDir: Boolean) {
+//        val hasPermission = checkStoragePermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, RC_STORAGE_PERMISSION)
+
+//        if (!hasPermission) {
+//            return
+//        }
+
+        val state = Environment.getExternalStorageState()
+        if (Environment.MEDIA_MOUNTED == state) {
+           val myDir = getExternalDir(privateDir)
+           try {
+               if (myDir?.exists() == false) {
+                   myDir.mkdir()
+               }
+
+               val txtFile = File(myDir, "arquivo.txt")
+
+               if(!txtFile.exists()) {
+                   txtFile.createNewFile()
+               }
+
+               val fos = FileOutputStream(txtFile)
+               save(fos)
+
+           } catch (e: Exception) {
+               Log.d("NGVL", "saveToExternal: Erro ao salvar arquivo", e)
+           }
+        } else {
+            Log.e("NGVL", "saveToExternal: Não é possivel escrever no SD Card")
+        }
+    }
+
+    private fun save(fos: FileOutputStream) {
+        val lines = TextUtils.split(edtText.text.toString(), "\n")
+        val writer = PrintWriter(fos)
+        for (line in lines) {
+            writer.println(line)
+        }
+        writer.flush()
+        writer.close()
+        fos.close()
+    }
+
+    private fun load(fis: FileInputStream) {
+        val reader = BufferedReader(InputStreamReader(fis))
+        val sb = StringBuilder()
+        do {
+            val line = reader.readLine() ?: break
+            if (sb.isNotEmpty()) sb.append('\n')
+            sb.append(line)
+        } while (true)
+        reader.close()
+        fis.close()
+        txtText.text = sb.toString()
+    }
+
+    private fun checkStoragePermission(permission: String, requestCode: Int): Boolean {
+
+        if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                Toast.makeText(this, R.string.message_permission_request, Toast.LENGTH_SHORT).show()
+            }
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+            return false
+        }
+
+        return true
+    }
+}
